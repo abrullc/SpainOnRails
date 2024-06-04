ebd618e29d905afd8b1755a51ae3d4d29af5d514
abrullc <adrianbrullcontreras@gmail.com>
2024-06-05 01:30:05 +0200

    Creada funcionalidad y diseño para mostrar mapa de Google Maps en HomeFragment

--- app/src/main/java/com/abrullc/spainonrails/homeModule/HomeFragment.kt
+++ app/src/main/java/com/abrullc/spainonrails/homeModule/HomeFragment.kt
@@ -6,13 +6,35 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.abrullc.spainonrails.R
+import com.abrullc.spainonrails.databinding.FragmentHomeBinding
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapView
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(), OnMapReadyCallback {
+    private lateinit var mBinding: FragmentHomeBinding
+    private lateinit var mapView: MapView
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        mBinding = FragmentHomeBinding.inflate(inflater, container, false)
+
+        mapView = mBinding.mapView
+        mapView.onCreate(savedInstanceState)
+        mapView.getMapAsync(this)
+
+        return mBinding.root
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        googleMap.addMarker(
+            MarkerOptions()
+                .position(LatLng(0.0, 0.0))
+                .title("Test")
+        )
     }
 }
