fc584cd2b5d90a049400bb6741f690ddf824a39f
abrullc <adrianbrullcontreras@gmail.com>
2024-06-09 19:57:07 +0200

    Añadidas variables en el companion object del application para guardar datos cargados.

--- app/src/main/java/com/abrullc/spainonrails/SpainOnRailsApplication.kt
+++ app/src/main/java/com/abrullc/spainonrails/SpainOnRailsApplication.kt
@@ -2,6 +2,12 @@ package com.abrullc.spainonrails
 
 import android.app.Application
 import com.abrullc.spainonrails.common.utils.Constants
+import com.abrullc.spainonrails.retrofit.entities.Estacion
+import com.abrullc.spainonrails.retrofit.entities.Pasaje
+import com.abrullc.spainonrails.retrofit.entities.PlanViaje
+import com.abrullc.spainonrails.retrofit.entities.PuntoInteres
+import com.abrullc.spainonrails.retrofit.entities.Ruta
+import com.abrullc.spainonrails.retrofit.entities.Tren
 import com.abrullc.spainonrails.retrofit.entities.Usuario
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -15,5 +21,17 @@ class SpainOnRailsApplication: Application() {
 
         // Logged user
         lateinit var usuario: Usuario
+
+        // General application data
+        lateinit var trenes: MutableList<Tren>
+        lateinit var rutas: MutableList<Ruta>
+        lateinit var estaciones: MutableList<Estacion>
+        lateinit var puntosInteres: MutableList<PuntoInteres>
+        lateinit var pasajes: MutableList<Pasaje>
+        lateinit var planesViaje: MutableList<PlanViaje>
+
+        // User specific data
+        lateinit var pasajesUsuario: MutableList<Pasaje>
+        lateinit var planesViajeUsuario: MutableList<PlanViaje>
     }
 }
