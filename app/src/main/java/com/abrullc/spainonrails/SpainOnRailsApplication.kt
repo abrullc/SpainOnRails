51263a98ab67546cfc0fa1c9df3bec9812e1b895
abrullc <adrianbrullcontreras@gmail.com>
2024-06-09 19:36:45 +0200

    Objecto retrofit añadido al companion object del archivo application

--- app/src/main/java/com/abrullc/spainonrails/SpainOnRailsApplication.kt
+++ app/src/main/java/com/abrullc/spainonrails/SpainOnRailsApplication.kt
@@ -1,10 +1,19 @@
 package com.abrullc.spainonrails
 
 import android.app.Application
+import com.abrullc.spainonrails.common.utils.Constants
 import com.abrullc.spainonrails.retrofit.entities.Usuario
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class SpainOnRailsApplication: Application() {
     companion object {
+        val retrofit: Retrofit = Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        // Logged user
         lateinit var usuario: Usuario
     }
 }
