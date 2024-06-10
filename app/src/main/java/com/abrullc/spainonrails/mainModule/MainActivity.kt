01efc62035b1be6acab7e8ce56d45fd659c60752
abrullc <adrianbrullcontreras@gmail.com>
2024-06-10 13:26:28 +0200

    Modificado proceso de carga de datos en MainActivity

--- app/src/main/java/com/abrullc/spainonrails/mainModule/MainActivity.kt
+++ app/src/main/java/com/abrullc/spainonrails/mainModule/MainActivity.kt
@@ -1,14 +1,13 @@
 package com.abrullc.spainonrails.mainModule
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.lifecycleScope
 import com.abrullc.spainonrails.R
 import com.abrullc.spainonrails.SpainOnRailsApplication
+import com.abrullc.spainonrails.common.utils.CommonFunctions
 import com.abrullc.spainonrails.databinding.ActivityMainBinding
 import com.abrullc.spainonrails.homeModule.HomeFragment
 import com.abrullc.spainonrails.retrofit.services.EstacionService
@@ -22,27 +21,29 @@ import com.abrullc.spainonrails.stationsModule.StationsFragment
 import com.abrullc.spainonrails.trainsModule.TrainsFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationView
-import kotlinx.coroutines.launch
-import retrofit2.HttpException
 
 class MainActivity : AppCompatActivity() {
     private lateinit var mBinding: ActivityMainBinding
     private lateinit var bottomNavigationView: BottomNavigationView
     private lateinit var drawerLayout: DrawerLayout
     private lateinit var navigationView: NavigationView
+    private lateinit var commonFunctions: CommonFunctions
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mBinding.root)
 
-        loadData()
-        initComponents()
-        initListeners()
-        replaceFragment(HomeFragment())
+        commonFunctions = CommonFunctions()
+
+        loadData {
+            initComponents()
+            initListeners()
+            replaceFragment(HomeFragment())
+        }
     }
 
-    private fun loadData() {
+    private fun loadData(onDataLoaded: () -> Unit) {
         val trenService = SpainOnRailsApplication.retrofit.create(TrenService::class.java)
         val rutaService = SpainOnRailsApplication.retrofit.create(RutaService::class.java)
         val estacionService = SpainOnRailsApplication.retrofit.create(EstacionService::class.java)
@@ -50,70 +51,78 @@ class MainActivity : AppCompatActivity() {
         val pasajeService = SpainOnRailsApplication.retrofit.create(PasajeService::class.java)
         val planViajeService = SpainOnRailsApplication.retrofit.create(PlanViajeService::class.java)
 
-        lifecycleScope.launch {
-            try {
-                // General application data
-                val resultTrenes = trenService.getTrenes()
-                val resultRutas = rutaService.getRutas()
-                val resultEstaciones = estacionService.getEstaciones()
-                val resultPuntoInteres = puntoInteresService.getPuntosInteres()
-                val resultPasajes = pasajeService.getPasajes()
-                val resultPlanesViaje = planViajeService.getPlanesViaje()
+        // General application data
+        commonFunctions.launchLifeCycleScope({
+            val resultTrenes = trenService.getTrenes().body()!!
+            SpainOnRailsApplication.trenes = resultTrenes
+        },this, this)
 
-                SpainOnRailsApplication.trenes = resultTrenes.body()!!
+        commonFunctions.launchLifeCycleScope({
+            val resultRutas = rutaService.getRutas()
 
-                SpainOnRailsApplication.rutas = resultRutas.body()!!
-                for (ruta in SpainOnRailsApplication.rutas) {
-                    val resultEstacionesRuta = rutaService.getEstacionesRuta(ruta.id)
-                    if (resultEstacionesRuta.body()!!.isNotEmpty()) {
-                        ruta.estaciones = resultEstacionesRuta.body()!!
-                    }
-                }
-
-                SpainOnRailsApplication.estaciones = resultEstaciones.body()!!
-                for (estacion in SpainOnRailsApplication.estaciones) {
-                    val resultPuntosInteresEstacion = puntoInteresService.getPuntosInteresEstacion(estacion.id)
-                    if (resultPuntosInteresEstacion.body()!!.isNotEmpty()) {
-                        estacion.puntosInteres = resultPuntosInteresEstacion.body()!!
-                    }
-                }
-
-                SpainOnRailsApplication.puntosInteres = resultPuntoInteres.body()!!
-
-                SpainOnRailsApplication.pasajes = resultPasajes.body()!!
-
-                SpainOnRailsApplication.planesViaje = resultPlanesViaje.body()!!
-                for (planViaje in SpainOnRailsApplication.planesViaje) {
-                    val resultVisitasPlanViaje = planViajeService.getVisitasPlanViaje(planViaje.id)
-                    if (resultVisitasPlanViaje.body()!!.isNotEmpty()) {
-                        planViaje.visitas = resultVisitasPlanViaje.body()!!
-                    }
-                }
-
-                // User specific data
-                val resultPasajesUsuario = pasajeService.getPasajesUsuario(SpainOnRailsApplication.usuario.id)
-                val resultPlanesViajeUsuario = planViajeService.getPlanesViajeUsuario(SpainOnRailsApplication.usuario.id)
-
-                if (resultPasajesUsuario.body()!!.isNotEmpty()) {
-                    SpainOnRailsApplication.pasajesUsuario = resultPasajesUsuario.body()!!
-                }
-
-                if (resultPlanesViajeUsuario.body()!!.isNotEmpty()) {
-                    SpainOnRailsApplication.planesViajeUsuario = resultPlanesViajeUsuario.body()!!
-                }
-
-            } catch (e: Exception) {
-                (e as? HttpException)?.let {
-                    when(it.code()) {
-                        400 -> {
-                            Toast.makeText(this@MainActivity, R.string.main_error_server, Toast.LENGTH_SHORT).show()
-                        }
-                        else ->
-                            Toast.makeText(this@MainActivity, R.string.unknown_error_server, Toast.LENGTH_SHORT).show()
-                    }
+            SpainOnRailsApplication.rutas = resultRutas.body()!!
+            for (ruta in SpainOnRailsApplication.rutas) {
+                val resultEstacionesRuta = rutaService.getEstacionesRuta(ruta.id)
+                if (resultEstacionesRuta.body()!!.isNotEmpty()) {
+                    ruta.estaciones = resultEstacionesRuta.body()!!
                 }
             }
-        }
+        }, this, this)
+
+        commonFunctions.launchLifeCycleScope({
+            val resultEstaciones = estacionService.getEstaciones()
+
+            SpainOnRailsApplication.estaciones = resultEstaciones.body()!!
+            for (estacion in SpainOnRailsApplication.estaciones) {
+                val resultPuntosInteresEstacion = puntoInteresService.getPuntosInteresEstacion(estacion.id)
+                if (resultPuntosInteresEstacion.body()!!.isNotEmpty()) {
+                    estacion.puntosInteres = resultPuntosInteresEstacion.body()!!
+                }
+            }
+        }, this, this)
+
+        commonFunctions.launchLifeCycleScope({
+            val resultPuntoInteres = puntoInteresService.getPuntosInteres()
+
+            SpainOnRailsApplication.puntosInteres = resultPuntoInteres.body()!!
+        }, this, this)
+
+        commonFunctions.launchLifeCycleScope({
+            val resultPasajes = pasajeService.getPasajes()
+
+            SpainOnRailsApplication.pasajes = resultPasajes.body()!!
+        }, this, this)
+
+        commonFunctions.launchLifeCycleScope({
+            val resultPlanesViaje = planViajeService.getPlanesViaje()
+
+            SpainOnRailsApplication.planesViaje = resultPlanesViaje.body()!!
+            for (planViaje in SpainOnRailsApplication.planesViaje) {
+                val resultVisitasPlanViaje = planViajeService.getVisitasPlanViaje(planViaje.id)
+                if (resultVisitasPlanViaje.body()!!.isNotEmpty()) {
+                    planViaje.visitas = resultVisitasPlanViaje.body()!!
+                }
+            }
+        }, this, this)
+
+        // User specific data
+        commonFunctions.launchLifeCycleScope({
+            val resultPasajesUsuario = pasajeService.getPasajesUsuario(SpainOnRailsApplication.usuario.id)
+
+            if (resultPasajesUsuario.body()!!.isNotEmpty()) {
+                SpainOnRailsApplication.pasajesUsuario = resultPasajesUsuario.body()!!
+            }
+        }, this, this)
+
+        commonFunctions.launchLifeCycleScope({
+            val resultPlanesViajeUsuario = planViajeService.getPlanesViajeUsuario(SpainOnRailsApplication.usuario.id)
+
+            if (resultPlanesViajeUsuario.body()!!.isNotEmpty()) {
+                SpainOnRailsApplication.planesViajeUsuario = resultPlanesViajeUsuario.body()!!
+            }
+        }, this, this)
+
+        onDataLoaded()
     }
 
     private fun initListeners() {
