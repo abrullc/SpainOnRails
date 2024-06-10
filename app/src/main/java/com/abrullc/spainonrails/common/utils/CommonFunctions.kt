17d01a56631942dc7cc413daa55120280f2338b0
abrullc <adrianbrullcontreras@gmail.com>
2024-06-10 13:24:13 +0200

    Añadida función para lanzar corrutinas con lifecycleScope en CommonFunctions

--- app/src/main/java/com/abrullc/spainonrails/common/utils/CommonFunctions.kt
+++ app/src/main/java/com/abrullc/spainonrails/common/utils/CommonFunctions.kt
@@ -2,8 +2,13 @@ package com.abrullc.spainonrails.common.utils
 
 import android.content.Context
 import android.webkit.URLUtil
+import android.widget.Toast
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.lifecycleScope
 import com.abrullc.spainonrails.R
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
 
 class CommonFunctions {
     fun validateURL(url: String): Boolean {
@@ -17,5 +22,27 @@ class CommonFunctions {
             .setPositiveButton(R.string.dialog_confirm, null)
             .setCancelable(false)
             .show()
+    }
+
+    fun launchLifeCycleScope(
+        code: suspend () -> Unit,
+        lifecycleOwner: LifecycleOwner,
+        context: Context
+    ) {
+        lifecycleOwner.lifecycleScope.launch {
+            try {
+                code()
+            } catch (e: Exception) {
+                (e as? HttpException)?.let {
+                    when(it.code()) {
+                        400 -> {
+                            Toast.makeText(context, R.string.main_error_server, Toast.LENGTH_SHORT).show()
+                        }
+                        else ->
+                            Toast.makeText(context, R.string.unknown_error_server, Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
     }
 }
