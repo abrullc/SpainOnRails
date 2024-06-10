afa71d2eff9b4b70de5c70024aa9e4165f0b323d
abrullc <adrianbrullcontreras@gmail.com>
2024-06-10 13:22:47 +0200

    Configuración de código para recycler view en TrainsFragment

--- app/src/main/java/com/abrullc/spainonrails/trainsModule/TrainsFragment.kt
+++ app/src/main/java/com/abrullc/spainonrails/trainsModule/TrainsFragment.kt
@@ -5,14 +5,41 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.abrullc.spainonrails.R
+import com.abrullc.spainonrails.SpainOnRailsApplication
+import com.abrullc.spainonrails.databinding.FragmentTrainsBinding
+import com.abrullc.spainonrails.trainsModule.adapters.TrainsListAdapter
 
 class TrainsFragment : Fragment() {
+    private lateinit var mBinding: FragmentTrainsBinding
+    private lateinit var mTrainAdapter: TrainsListAdapter
+    private lateinit var mTrainLinearLayoutManager: RecyclerView.LayoutManager
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        mBinding = FragmentTrainsBinding.inflate(inflater, container, false)
+
+        setupRecyclerView()
+
+        return mBinding.root
+    }
+
+    private fun setupRecyclerView() {
+        mTrainAdapter = TrainsListAdapter()
+        mTrainAdapter.submitList(SpainOnRailsApplication.trenes)
+
+        mTrainLinearLayoutManager = LinearLayoutManager(this.context, LinearLayoutManager.VERTICAL, false)
+
+        with(mBinding) {
+            rcvTrains.apply {
+                setHasFixedSize(true)
+                layoutManager = mTrainLinearLayoutManager
+                adapter = mTrainAdapter
+            }
+        }
     }
 }
