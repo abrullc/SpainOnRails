8522d54dbe03e435d2ec4c41b35084cd6487216f
abrullc <adrianbrullcontreras@gmail.com>
2024-06-11 01:57:12 +0200

    Corregido código erróneo para el setup del recycler view en StationsFragment. Implementada funcionalidad de la SearchView

--- app/src/main/java/com/abrullc/spainonrails/stationsModule/StationsFragment.kt
+++ app/src/main/java/com/abrullc/spainonrails/stationsModule/StationsFragment.kt
@@ -5,14 +5,69 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.abrullc.spainonrails.R
+import android.widget.SearchView
+import androidx.core.widget.addTextChangedListener
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.abrullc.spainonrails.SpainOnRailsApplication
+import com.abrullc.spainonrails.databinding.FragmentStationsBinding
+import com.abrullc.spainonrails.databinding.FragmentTrainsBinding
+import com.abrullc.spainonrails.stationsModule.adapters.StationsListAdapter
+import com.google.android.material.search.SearchView.TransitionState
 
 class StationsFragment : Fragment() {
+    private lateinit var mBinding: FragmentStationsBinding
+    private lateinit var mStationAdapter: StationsListAdapter
+    private lateinit var mStationLinearLayoutManager: RecyclerView.LayoutManager
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_stations, container, false)
+        mBinding = FragmentStationsBinding.inflate(inflater, container, false)
+
+        setupRecyclerView()
+
+        setupSearchView()
+
+        return mBinding.root
+    }
+
+    private fun setupRecyclerView() {
+        mStationAdapter = StationsListAdapter()
+        mStationAdapter.submitList(SpainOnRailsApplication.estaciones)
+
+        mStationLinearLayoutManager = LinearLayoutManager(this.context, LinearLayoutManager.VERTICAL, false)
+
+        with(mBinding) {
+            rcvStations.apply {
+                setHasFixedSize(true)
+                layoutManager = mStationLinearLayoutManager
+                adapter = mStationAdapter
+            }
+        }
+    }
+
+    private fun setupSearchView() {
+        mBinding.svStations.setOnQueryTextListener(
+            object: SearchView.OnQueryTextListener {
+                override fun onQueryTextSubmit(query: String?): Boolean {
+                    return false
+                }
+
+                override fun onQueryTextChange(newText: String): Boolean {
+                    getStationsByFilter(newText)
+                    return true
+                }
+            }
+        )
+    }
+
+    private fun getStationsByFilter(filter: String) {
+        val filteredStations = SpainOnRailsApplication.estaciones.filter {
+            it.nombre.contains(filter, ignoreCase = true)
+        }
+
+        mStationAdapter.submitList(filteredStations)
     }
 }
