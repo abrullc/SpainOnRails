7fdd82b5b1480ed054dd65fe3dd7d1803007c923
abrullc <adrianbrullcontreras@gmail.com>
2024-06-12 01:25:08 +0200

    Agregada corrutina para la carga de trenes en TrainsFragment

--- app/src/main/java/com/abrullc/spainonrails/trainsModule/TrainsFragment.kt
+++ app/src/main/java/com/abrullc/spainonrails/trainsModule/TrainsFragment.kt
@@ -8,11 +8,16 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.abrullc.spainonrails.SpainOnRailsApplication
+import com.abrullc.spainonrails.common.utils.CommonFunctions
 import com.abrullc.spainonrails.databinding.FragmentTrainsBinding
+import com.abrullc.spainonrails.retrofit.services.TrenService
 import com.abrullc.spainonrails.trainsModule.adapters.TrainsListAdapter
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 class TrainsFragment : Fragment() {
     private lateinit var mBinding: FragmentTrainsBinding
+    private lateinit var commonFunctions: CommonFunctions
     private lateinit var mTrainAdapter: TrainsListAdapter
     private lateinit var mTrainLinearLayoutManager: RecyclerView.LayoutManager
 
@@ -22,14 +27,29 @@ class TrainsFragment : Fragment() {
     ): View? {
         mBinding = FragmentTrainsBinding.inflate(inflater, container, false)
 
+        commonFunctions = CommonFunctions()
+
+        getTrenes()
+
         setupRecyclerView()
 
         return mBinding.root
     }
 
+    private fun getTrenes() {
+        commonFunctions.launchLifeCycleScope({
+            val trenService = SpainOnRailsApplication.retrofit.create(TrenService::class.java)
+
+            val resultTrenes = trenService.getTrenes().body()!!
+
+            withContext(Dispatchers.Main) {
+                mTrainAdapter.submitList(resultTrenes)
+            }
+        }, this, requireContext())
+    }
+
     private fun setupRecyclerView() {
         mTrainAdapter = TrainsListAdapter()
-        mTrainAdapter.submitList(SpainOnRailsApplication.trenes)
 
         mTrainLinearLayoutManager = LinearLayoutManager(this.context, LinearLayoutManager.VERTICAL, false)
 
