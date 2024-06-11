a8393335cff2930be6fe449e6578412c7a26a036
abrullc <adrianbrullcontreras@gmail.com>
2024-06-12 01:23:41 +0200

    Agregada corrutina para cargar estaciones al lanzar el mapa y cálculo de posición predeterminada de la cámara

--- app/src/main/java/com/abrullc/spainonrails/homeModule/HomeFragment.kt
+++ app/src/main/java/com/abrullc/spainonrails/homeModule/HomeFragment.kt
@@ -5,15 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.abrullc.spainonrails.SpainOnRailsApplication
+import com.abrullc.spainonrails.common.utils.CommonFunctions
 import com.abrullc.spainonrails.databinding.FragmentHomeBinding
+import com.abrullc.spainonrails.retrofit.services.EstacionService
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.MapView
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.LatLngBounds
 import com.google.android.gms.maps.model.MarkerOptions
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 class HomeFragment : Fragment(), OnMapReadyCallback {
     private lateinit var mBinding: FragmentHomeBinding
+    private lateinit var commonFunctions: CommonFunctions
     private lateinit var mapView: MapView
 
     override fun onCreateView(
@@ -21,6 +29,8 @@ class HomeFragment : Fragment(), OnMapReadyCallback {
         savedInstanceState: Bundle?
     ): View? {
         mBinding = FragmentHomeBinding.inflate(inflater, container, false)
+
+        commonFunctions = CommonFunctions()
 
         mapView = mBinding.mapView
         mapView.onCreate(savedInstanceState)
@@ -30,10 +40,28 @@ class HomeFragment : Fragment(), OnMapReadyCallback {
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
-        googleMap.addMarker(
-            MarkerOptions()
-                .position(LatLng(0.0, 0.0))
-                .title("Test")
-        )
+        commonFunctions.launchLifeCycleScope({
+            val estacionService = SpainOnRailsApplication.retrofit.create(EstacionService::class.java)
+
+            val resultEstaciones = estacionService.getEstaciones()
+
+            SpainOnRailsApplication.estaciones = resultEstaciones.body()!!
+
+            withContext(Dispatchers.Main) {
+                val boundsBuilder = LatLngBounds.Builder()
+
+                for (estacion in SpainOnRailsApplication.estaciones) {
+                    val latLng = LatLng(estacion.longitud.toDouble(), estacion.latitud.toDouble())
+                    boundsBuilder.include(latLng)
+                    googleMap.addMarker(
+                        MarkerOptions()
+                            .position(latLng)
+                            .title(estacion.nombre)
+                    )
+                }
+
+                googleMap.moveCamera(CameraUpdateFactory.newLatLngBounds(boundsBuilder.build(), 500, 500, 0))
+            }
+        }, this, requireContext())
     }
 }
