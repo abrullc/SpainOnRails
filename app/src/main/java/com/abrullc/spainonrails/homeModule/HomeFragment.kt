2d2ee05d2d4a20bd3c8fb35f9361be5ea6403ec5
abrullc <adrianbrullcontreras@gmail.com>
2024-06-12 02:14:25 +0200

    Añadidas localizaciones de los puntos de interés al mapa en HomeFragment. Especificados colores para cada lugar: naranja para estaciones, amarillo para puntos de interés

--- app/src/main/java/com/abrullc/spainonrails/homeModule/HomeFragment.kt
+++ app/src/main/java/com/abrullc/spainonrails/homeModule/HomeFragment.kt
@@ -9,10 +9,12 @@ import com.abrullc.spainonrails.SpainOnRailsApplication
 import com.abrullc.spainonrails.common.utils.CommonFunctions
 import com.abrullc.spainonrails.databinding.FragmentHomeBinding
 import com.abrullc.spainonrails.retrofit.services.EstacionService
+import com.abrullc.spainonrails.retrofit.services.PuntoInteresService
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.MapView
 import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.LatLngBounds
 import com.google.android.gms.maps.model.MarkerOptions
@@ -42,21 +44,35 @@ class HomeFragment : Fragment(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         commonFunctions.launchLifeCycleScope({
             val estacionService = SpainOnRailsApplication.retrofit.create(EstacionService::class.java)
+            val puntoInteresService = SpainOnRailsApplication.retrofit.create(PuntoInteresService::class.java)
 
             val resultEstaciones = estacionService.getEstaciones()
-
-            SpainOnRailsApplication.estaciones = resultEstaciones.body()!!
+            val resultPuntoInteres = puntoInteresService.getPuntosInteres()
 
             withContext(Dispatchers.Main) {
+                SpainOnRailsApplication.estaciones = resultEstaciones.body()!!
+
                 val boundsBuilder = LatLngBounds.Builder()
 
-                for (estacion in SpainOnRailsApplication.estaciones) {
+                for (estacion in resultEstaciones.body()!!) {
                     val latLng = LatLng(estacion.longitud.toDouble(), estacion.latitud.toDouble())
                     boundsBuilder.include(latLng)
                     googleMap.addMarker(
                         MarkerOptions()
                             .position(latLng)
                             .title(estacion.nombre)
+                            .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE))
+                    )
+                }
+
+                for (puntoInteres in resultPuntoInteres.body()!!) {
+                    val latLng = LatLng(puntoInteres.longitud.toDouble(), puntoInteres.latitud.toDouble())
+                    boundsBuilder.include(latLng)
+                    googleMap.addMarker(
+                        MarkerOptions()
+                            .position(latLng)
+                            .title(puntoInteres.nombre)
+                            .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_YELLOW))
                     )
                 }
 
