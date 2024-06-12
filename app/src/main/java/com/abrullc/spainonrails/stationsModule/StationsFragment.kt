ca7acdaf5f0f4e22389e32c7cd20005f62ab9443
abrullc <adrianbrullcontreras@gmail.com>
2024-06-12 17:50:59 +0200

    Agregada corrutina para la carga de estaciones en StationsFragment

--- app/src/main/java/com/abrullc/spainonrails/stationsModule/StationsFragment.kt
+++ app/src/main/java/com/abrullc/spainonrails/stationsModule/StationsFragment.kt
@@ -9,13 +9,20 @@ import android.widget.SearchView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.abrullc.spainonrails.SpainOnRailsApplication
+import com.abrullc.spainonrails.common.utils.CommonFunctions
 import com.abrullc.spainonrails.databinding.FragmentStationsBinding
+import com.abrullc.spainonrails.retrofit.entities.Estacion
+import com.abrullc.spainonrails.retrofit.services.EstacionService
 import com.abrullc.spainonrails.stationsModule.adapters.StationsListAdapter
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 class StationsFragment : Fragment() {
     private lateinit var mBinding: FragmentStationsBinding
+    private lateinit var commonFunctions: CommonFunctions
     private lateinit var mStationAdapter: StationsListAdapter
     private lateinit var mStationLinearLayoutManager: RecyclerView.LayoutManager
+    private lateinit var listaEstaciones: MutableList<Estacion>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,16 +30,31 @@ class StationsFragment : Fragment() {
     ): View? {
         mBinding = FragmentStationsBinding.inflate(inflater, container, false)
 
-        setupRecyclerView()
+        commonFunctions = CommonFunctions()
 
+        getEstaciones()
+
+        setupRecyclerView()
         setupSearchView()
 
         return mBinding.root
     }
 
+    private fun getEstaciones() {
+        commonFunctions.launchLifeCycleScope({
+            val estacionService = SpainOnRailsApplication.retrofit.create(EstacionService::class.java)
+
+            val resultEstaciones = estacionService.getEstaciones()
+
+            withContext(Dispatchers.Main) {
+                mStationAdapter.submitList(resultEstaciones.body()!!)
+                listaEstaciones = resultEstaciones.body()!!
+            }
+        }, this, requireContext())
+    }
+
     private fun setupRecyclerView() {
         mStationAdapter = StationsListAdapter()
-        mStationAdapter.submitList(SpainOnRailsApplication.estaciones)
 
         mStationLinearLayoutManager = LinearLayoutManager(this.context, LinearLayoutManager.VERTICAL, false)
 
@@ -61,7 +83,7 @@ class StationsFragment : Fragment() {
     }
 
     private fun getStationsByFilter(filter: String) {
-        val filteredStations = SpainOnRailsApplication.estaciones.filter {
+        val filteredStations = listaEstaciones.filter {
             it.nombre.contains(filter, ignoreCase = true)
         }
 
