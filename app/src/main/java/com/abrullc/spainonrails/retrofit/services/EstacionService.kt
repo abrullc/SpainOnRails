a752f7f38a87c6524e591c4ef1ba93da8d208fb0
abrullc <adrianbrullcontreras@gmail.com>
2024-06-14 04:11:48 +0200

    Modificado el tipo de salida de endpoint en EstacionService y creado uno nuevo en el mismo servicio

--- app/src/main/java/com/abrullc/spainonrails/retrofit/services/EstacionService.kt
+++ app/src/main/java/com/abrullc/spainonrails/retrofit/services/EstacionService.kt
@@ -2,6 +2,7 @@ package com.abrullc.spainonrails.retrofit.services
 
 import com.abrullc.spainonrails.common.utils.Constants
 import com.abrullc.spainonrails.retrofit.entities.Estacion
+import com.abrullc.spainonrails.retrofit.entities.Ruta
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
@@ -11,5 +12,8 @@ interface EstacionService {
     suspend fun getEstaciones(): Response<MutableList<Estacion>>
 
     @GET(Constants.ESTACION_PATH)
-    suspend fun getEstacion(@Path("id") id: Int): Estacion
+    suspend fun getEstacion(@Path("id") id: Int): Response<Estacion>
+
+    @GET(Constants.RUTAS_ESTACION_PATH)
+    suspend fun getRutasEstacion(@Path("id") id: Int): Response<MutableList<Ruta>>
 }
