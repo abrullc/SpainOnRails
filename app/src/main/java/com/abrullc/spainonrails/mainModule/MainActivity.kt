7c56e6600def4785f02f150c31f1052b8a639b1b
abrullc <adrianbrullcontreras@gmail.com>
2024-06-15 22:43:58 +0200

    Añadidos nuevos listeners y carga de imagen de usuario en MainActivity

--- app/src/main/java/com/abrullc/spainonrails/mainModule/MainActivity.kt
+++ app/src/main/java/com/abrullc/spainonrails/mainModule/MainActivity.kt
@@ -1,17 +1,22 @@
 package com.abrullc.spainonrails.mainModule
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
 import com.abrullc.spainonrails.R
+import com.abrullc.spainonrails.SpainOnRailsApplication
 import com.abrullc.spainonrails.common.utils.CommonFunctions
 import com.abrullc.spainonrails.databinding.ActivityMainBinding
 import com.abrullc.spainonrails.mainModule.homeModule.HomeFragment
 import com.abrullc.spainonrails.mainModule.routesModule.RoutesFragment
 import com.abrullc.spainonrails.mainModule.stationsModule.StationsFragment
 import com.abrullc.spainonrails.mainModule.trainsModule.TrainsFragment
+import com.abrullc.spainonrails.userModule.UserActivity
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationView
 
@@ -35,29 +40,54 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initListeners() {
-        mBinding.imgNavigationDrawer.setOnClickListener {
-            drawerLayout.openDrawer(GravityCompat.START)
+        with(mBinding) {
+            imgNavigationDrawer.setOnClickListener {
+                drawerLayout.openDrawer(GravityCompat.START)
+            }
+
+            imgProfile.setOnClickListener { goToUserProfile() }
+
+            Glide.with(this@MainActivity)
+                .load(SpainOnRailsApplication.usuario.imagen)
+                .placeholder(R.drawable.ic_profile)
+                .error(R.drawable.ic_profile)
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .centerCrop()
+                .circleCrop()
+                .into(imgProfile)
         }
 
         navigationView.setNavigationItemSelectedListener { menuItem ->
             when (menuItem.itemId) {
+                R.id.drawer_profile -> {
+                    goToUserProfile()
+                }
+
                 R.id.drawer_home -> {
                     replaceFragment(HomeFragment(), getString(R.string.home))
                     bottomNavigationView.menu.findItem(R.id.bottom_home).isChecked = true
                 }
+
                 R.id.drawer_trains -> {
                     replaceFragment(TrainsFragment(), getString(R.string.trains))
                     bottomNavigationView.menu.findItem(R.id.bottom_trains).isChecked = true
                 }
+
                 R.id.drawer_routes -> {
                     replaceFragment(RoutesFragment(), getString(R.string.routes))
                     bottomNavigationView.menu.findItem(R.id.bottom_routes).isChecked = true
                 }
+
                 R.id.drawer_stations -> {
                     replaceFragment(StationsFragment(), getString(R.string.stations))
                     bottomNavigationView.menu.findItem(R.id.bottom_stations).isChecked = true
                 }
+
+                R.id.drawer_logout -> {
+                    TODO("Implementar función de log out")
+                }
             }
+
             navigationView.setCheckedItem(menuItem.itemId)
             drawerLayout.closeDrawer(GravityCompat.START)
             true
@@ -69,22 +99,31 @@ class MainActivity : AppCompatActivity() {
                     replaceFragment(HomeFragment(), getString(R.string.home))
                     navigationView.setCheckedItem(R.id.drawer_home)
                 }
+
                 R.id.bottom_trains -> {
                     replaceFragment(TrainsFragment(), getString(R.string.trains))
                     navigationView.setCheckedItem(R.id.drawer_trains)
                 }
+
                 R.id.bottom_routes -> {
                     replaceFragment(RoutesFragment(), getString(R.string.routes))
                     navigationView.setCheckedItem(R.id.drawer_routes)
                 }
+
                 R.id.bottom_stations -> {
                     replaceFragment(StationsFragment(), getString(R.string.stations))
                     navigationView.setCheckedItem(R.id.drawer_stations)
                 }
             }
+
             menuItem.isChecked = true
             true
         }
+    }
+
+    private fun goToUserProfile() {
+        val intent = Intent(this, UserActivity::class.java)
+        startActivity(intent)
     }
 
     private fun replaceFragment(fragment: Fragment, title: String) {
