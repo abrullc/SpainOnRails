8a934d552e6ce3dc64d98708b0b1102766cfb556
abrullc <adrianbrullcontreras@gmail.com>
2024-06-15 12:13:35 +0200

    Modificada carga de datos en StationDetailFragment para que también se muestren puntos de interés de la estación en el mapa

--- app/src/main/java/com/abrullc/spainonrails/details/stationDetailModule/StationDetailFragment.kt
+++ app/src/main/java/com/abrullc/spainonrails/details/stationDetailModule/StationDetailFragment.kt
@@ -15,7 +15,9 @@ import com.abrullc.spainonrails.databinding.FragmentStationDetailBinding
 import com.abrullc.spainonrails.details.routeDetailModule.RouteDetailFragment
 import com.abrullc.spainonrails.mainModule.routesModule.adapters.RoutesListAdapter
 import com.abrullc.spainonrails.retrofit.entities.Estacion
+import com.abrullc.spainonrails.retrofit.entities.PuntoInteres
 import com.abrullc.spainonrails.retrofit.services.EstacionService
+import com.abrullc.spainonrails.retrofit.services.PuntoInteresService
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.google.android.gms.maps.CameraUpdateFactory
@@ -31,6 +33,7 @@ import kotlinx.coroutines.withContext
 class StationDetailFragment : Fragment(), OnMapReadyCallback, OnClickListener {
     private var idEstacion: Int = 0
     private lateinit var estacion: Estacion
+    private lateinit var puntosInteresEstacion: MutableList<PuntoInteres>
     private lateinit var commonFunctions: CommonFunctions
     private lateinit var mBinding: FragmentStationDetailBinding
     private var savedInstanceState: Bundle? = null
@@ -62,17 +65,21 @@ class StationDetailFragment : Fragment(), OnMapReadyCallback, OnClickListener {
 
         this.savedInstanceState = savedInstanceState
 
-        getEstacion()
+        getEstacionData()
     }
 
-    private fun getEstacion() {
+    private fun getEstacionData() {
         commonFunctions.launchLifeCycleScope({
             val estacionService = SpainOnRailsApplication.retrofit.create(EstacionService::class.java)
+            val puntoInteresService = SpainOnRailsApplication.retrofit.create(PuntoInteresService::class.java)
 
             val resultEstacion = estacionService.getEstacion(idEstacion).body()!!
+            val resultPuntosInteresEstacion = puntoInteresService.getPuntosInteresEstacion(idEstacion).body()!!
 
             withContext(Dispatchers.Main) {
                 estacion = resultEstacion
+                puntosInteresEstacion = resultPuntosInteresEstacion
+
                 setupEstacionInfo()
 
                 mapView = mBinding.mapView
@@ -101,15 +108,25 @@ class StationDetailFragment : Fragment(), OnMapReadyCallback, OnClickListener {
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
-        val latLng = LatLng(estacion.latitud.toDouble(), estacion.longitud.toDouble())
+        val stationLatLng = LatLng(estacion.latitud.toDouble(), estacion.longitud.toDouble())
         googleMap.addMarker(
             MarkerOptions()
-                .position(latLng)
+                .position(stationLatLng)
                 .title(estacion.nombre)
                 .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE))
         )
 
-        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 12.0f))
+        for (puntoInteresEstacion in puntosInteresEstacion) {
+            val latLng = LatLng(puntoInteresEstacion.latitud.toDouble(), puntoInteresEstacion.longitud.toDouble())
+            googleMap.addMarker(
+                MarkerOptions()
+                    .position(latLng)
+                    .title(puntoInteresEstacion.nombre)
+                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_YELLOW))
+            )
+        }
+
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(stationLatLng, 12.0f))
         googleMap.uiSettings.isScrollGesturesEnabled = false
         googleMap.uiSettings.isRotateGesturesEnabled = false
     }
