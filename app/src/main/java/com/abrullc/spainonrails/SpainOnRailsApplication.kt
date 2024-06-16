2adb405f641255ed2eeb3bc17159353abd0d5700
abrullc <adrianbrullcontreras@gmail.com>
2024-06-16 13:51:50 +0200

    Creado y añadido constructor Gson con serialización de nulos al constructor de retrofit en SpainOnRailsApplication

--- app/src/main/java/com/abrullc/spainonrails/SpainOnRailsApplication.kt
+++ app/src/main/java/com/abrullc/spainonrails/SpainOnRailsApplication.kt
@@ -3,18 +3,24 @@ package com.abrullc.spainonrails
 import android.app.Application
 import com.abrullc.spainonrails.common.utils.Constants
 import com.abrullc.spainonrails.retrofit.entities.Usuario
+import com.google.gson.GsonBuilder
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class SpainOnRailsApplication: Application() {
     companion object {
+        private val gson = GsonBuilder()
+            .serializeNulls()
+            .create()
+
         val retrofit: Retrofit = Retrofit.Builder()
             .baseUrl(Constants.BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create(gson))
             .build()
 
         // Logged user
         lateinit var usuario: Usuario
         var isUsuarioUpdated = false
+        var isLogoutRequired = false
     }
 }
